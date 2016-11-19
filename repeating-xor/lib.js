da7945b7fbe722654fc31c11b1b325e5116559a8
Henry Feng <henryzhefeng@gmail.com>
2016-11-19 15:33:28 +0800

    add code for repeating-xor

---
+++ repeating-xor/lib.js
@@ -0,0 +1,54 @@
+'use strict';
+
+function hex2arr(hexstr) {
+    const arr = [];
+    for (let i = 0; i + 1 < hexstr.length; i += 2) {
+        arr.push(parseInt(hexstr[i] + hexstr[i + 1], 16));
+    }
+    return arr;
+}
+
+function arr2char(arr) {
+    let result = '';
+    for (const val of arr) {
+        result += String.fromCharCode(val);
+    }
+    return result;
+}
+
+function listAll(arrOfArr) {
+    const current = [];
+    const result = [];
+
+    function traverse(index) {
+        if (index < arrOfArr.length) {
+            const arr = arrOfArr[index];
+            for (const val of arr) {
+                current.push(val);
+                traverse(index + 1);
+                current.pop();
+            }
+        } else { // index===result.length
+            result.push(current.slice());
+        }
+    }
+    traverse(0);
+    return result;
+}
+
+function discrypt(ciphertext, key) {
+    let longkey = [];
+    while (longkey.length < ciphertext.length) {
+        longkey = longkey.concat(key);
+    }
+    const xored = [];
+    for (let i = 0; i < ciphertext.length; i++) {
+        xored.push(longkey[i] ^ ciphertext[i]);
+    }
+    return arr2char(xored);
+}
+
+module.exports.hex2arr = hex2arr;
+module.exports.arr2char = arr2char;
+module.exports.listAll = listAll;
+module.exports.discrypt = discrypt;
--- repeating-xor/main.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const fs = require('fs');
-
-const encrypted = fs.readFileSync('./encrypted').toString();
-
-console.log(encrypted.length);
