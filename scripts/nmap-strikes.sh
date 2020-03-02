f39f51d1588ceaf0a9c4c3c3f554860479d657ed
zhefeng <zhef@umich.edu>
2020-03-01 18:10:45 -0800

    add -Pn to the nmap scripts.

--- scripts/nmap-strikes.sh
+++ scripts/nmap-strikes.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ip=$1
 mkdir nmap 2>/dev/null
-nmap -sC -sV -oA nmap/comm-tcp $ip &>/dev/null &
-nmap -p- -sV $ip > nmap/all-tcp &
-nmap -sU $ip > nmap/comm-udp &
-nmap -p- -sU $ip > nmap/all-udp &
-nmap --script=vuln $ip > nmap/vuln &
+nmap -sC -sV -oA -Pn nmap/comm-tcp $ip &>/dev/null &
+nmap -p- -sV -Pn $ip > nmap/all-tcp &
+nmap -sU -Pn $ip > nmap/comm-udp &
+nmap -p- -sU -Pn $ip > nmap/all-udp &
+nmap --script=vuln -Pn $ip > nmap/vuln &
