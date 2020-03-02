14028b3a4a11ad0a7904737f5df1e681d8d4120e
zhefeng <zhef@umich.edu>
2020-03-01 22:45:29 -0800

    Fix a bug causing a failed nmap scan.

--- scripts/nmap-strikes.sh
+++ scripts/nmap-strikes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ip=$1
 mkdir nmap 2>/dev/null
-nmap -sC -sV -oA -Pn nmap/comm-tcp $ip &>/dev/null &
+nmap -sC -Pn -sV -oA nmap/comm-tcp $ip &>/dev/null &
 nmap -p- -sV -Pn $ip > nmap/all-tcp &
 nmap -sU -Pn $ip > nmap/comm-udp &
 nmap -p- -sU -Pn $ip > nmap/all-udp &
