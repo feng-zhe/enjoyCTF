663f07fa110a59182771843b8e92fb72a1d29a6d
zhefeng <zhef@umich.edu>
2020-03-22 23:49:00 -0700

    update nmap-strikes.sh

--- scripts/nmap-strikes.sh
+++ scripts/nmap-strikes.sh
@@ -4,5 +4,5 @@ mkdir nmap 2>/dev/null
 nmap -sC -Pn -sV -oA nmap/comm-tcp $ip &>/dev/null &
 nmap -p- -sV -Pn $ip > nmap/all-tcp &
 nmap -sU -Pn $ip > nmap/comm-udp &
-nmap -p- -sU -Pn $ip > nmap/all-udp &
+#nmap -p- -sU -Pn $ip > nmap/all-udp &
 nmap --script=vuln -Pn $ip > nmap/vuln &
